18ea45f44c6449b16255a48676bf732ce8ce6cd1
Daniyar Shulakov <dshulakov@gmail.com>
2019-11-05 12:33:38 +0600

    added db

--- src/main/java/com/example/demo1/DemoController.java
+++ src/main/java/com/example/demo1/DemoController.java
@@ -1,16 +1,27 @@
 package com.example.demo1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
+    private final CountryRepo countryRepo;
+
+    @Autowired
+    public DemoController(CountryRepo countryRepo) {
+        this.countryRepo = countryRepo;
+    }
+
     @GetMapping
-    public String demo() {
-        return "Hello from demo 1  after commit";
+    public List<Country> demo() {
+        List<Country> countryList = countryRepo.findAll();
+        return countryList;
     }
 
 }
